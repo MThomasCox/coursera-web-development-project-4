f7d2c0793481445d004c80bed45b99c756230638
MThomasCox <mthomasc@gmail.com>
2025-10-05 19:26:34 -0400

    Adding back to top button

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const toTopBtn = document.getElementById("toTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+};
+
+toTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
